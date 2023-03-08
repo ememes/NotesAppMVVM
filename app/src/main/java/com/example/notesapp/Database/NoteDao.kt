566f4e47c104822aa02e064980208b9bdba68dad
Mustafa Mert Selvi <mustafamertselvi@gmail.com>
2023-03-09 02:42:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/Database/NoteDao.kt
@@ -0,0 +1,20 @@
+package com.example.notesapp.Database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.notesapp.Models.Note
+
+@Dao
+interface NoteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note:Note)
+    @Delete
+    suspend fun delete(note: Note)
+    @Query("Select * from notes_table order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("UPDATE notes_table SET title=:title,note= :note WHERE id=:id")
+    suspend fun update(id: Int?, title:String?, note: String?)
+
+
+}
